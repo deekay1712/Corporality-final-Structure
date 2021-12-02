016959229e7a26390b2e92e371902ebc0fa854ad
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2021-12-02 23:14:23 +0530

    updated css

--- src/components/StrategyAndConsulting/Components/SACComp6.jsx
+++ src/components/StrategyAndConsulting/Components/SACComp6.jsx
@@ -24,15 +24,13 @@ function SACComp6() {
         data-aos-easing="ease-in-out"
       >
         <div className="SACC6_community_first_heading">
-          <div className="SACC6_community_first_heading_text">Community Support</div>
+          <div className="SACC6_community_first_heading_text">Deep Industry Expertise</div>
           <div className="SACC6_community_first_heading_arrow">
             <img src={heading_1_arrow} alt="" />
           </div>
         </div>
         <div className="SACC6_community_first_description">
-          We believe that education is an imperative tool for the growth of a
-          child. Corporality Global has been instrumental in propelling several
-          children towards a better living through education.
+        Corporality employs high-end resources that come with deep industry expertise and knowledge and use proven industry-oriented methods and solutions to let you benefit from remarkable contexts.
         </div>
         <div className="SACC6_community_top_image">
           <img src={img3} alt="" className="SACC6_community_top_image_image" />
@@ -52,16 +50,14 @@ function SACComp6() {
       >
         <div className="SACC6_community_second_heading">
           <div className="SACC6_community_second_heading_text">
-            Corporate Social Responsibility
+          Agility and Enterprise Resilience
           </div>
           <div className="SACC6_community_second_heading_arrow">
             <img src={heading_1_arrow} alt="" />
           </div>
         </div>
         <div className="SACC6_community_second_description">
-          As part of our CSR activities, Corporality Global is currently laying
-          a road map in the geospatial technology area to work closely with
-          authorities across geographical mapping and analysis.
+        Corporality fosters agility and builds enterprise resilience through continuous innovation.  We re-engineer functions in ways that suit both internal and external customer moods.  
         </div>
         <div className="SACC6_community_bottom_image">
           <img
